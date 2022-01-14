21fd7a57d3496fcce28295deaca284b85a076c2d
Andrea Lorenzon <andrea.lorenzon@ceric-eric.eu>
2022-01-14 14:41:46 +0100

    removed old token

--- main.py
+++ main.py
@@ -15,5 +15,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-#client.run(os.getenv('TOKEN'))
-client.run('OTMxNTMyNzkzOTEyMTMxNTg0.YeFzhQ.Wv7GOgjHv4gmEdfXPRzfFmKvMWM')
+client.run(os.getenv('TOKEN'))
