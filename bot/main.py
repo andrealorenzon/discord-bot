5935bc71e01f8170d03e27e49a31426195067b89
Andrea Lorenzon <andrea.lorenzon@ceric-eric.eu>
2022-01-14 15:59:38 +0100

    rm venv

--- bot/main.py
+++ bot/main.py
@@ -12,5 +12,24 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
+@bot.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+
 if __name__ == "__main__":
     bot.run(TOKEN)
